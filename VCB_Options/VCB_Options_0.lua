0a843834fe698c88ec7b267201d180b867eed7a7
TheTrueIndolent <panoulis26@gmail.com>
2024-11-07 15:48:57 +0200

    Update VCB_Options_0.lua

--- VCB_Options/VCB_Options_0.lua
+++ VCB_Options/VCB_Options_0.lua
@@ -1,23 +1,3 @@
--- taking care of the panel --
-vcbOptions0.BGtexture:SetAlpha(0.70)
-vcbOptions0.TopTxt:SetText("It would be good when you open the |cffFF0055Options' Panel|r to close the other ones so you can watch what changes you are making!")
-vcbOptions0.CenterTxt:SetText("|A:"..C_AddOns.GetAddOnMetadata("VCB", "IconAtlas")..":16:16|a "..C_AddOns.GetAddOnMetadata("VCB", "Title").." is an add on that enchants the default Casting Bars!|n|nPress the button below to open the options pane!")
-vcbOptions0.BottomTxt:SetText("Thank you for using this amazing add-on!|nYou are a |cff00CED1Funky|r and a |cffFF0055Groovy|r person!|nMay the good |cff9400D3Mojo|r be with you!")
--- button 1 to option's panel --
-vcbOptions0Button1.Text:SetText("Options Panel")
--- enter --
-vcbOptions0Button1:SetScript("OnEnter", function(self)
-	vcbEnteringMenus(self)
-	GameTooltip:SetText("|A:"..C_AddOns.GetAddOnMetadata("VCB", "IconAtlas")..":16:16|a "..vcbMainColor:WrapTextInColorCode(C_AddOns.GetAddOnMetadata("VCB", "Title")).."|nClick: "..vcbMainColor:WrapTextInColorCode("Open the main panel of settings!")) 
-end)
--- leave --
-vcbOptions0Button1:SetScript("OnLeave", vcbLeavingMenus)
--- click --
-vcbOptions0Button1:HookScript("OnClick", function(self, button, down)
-	if button == "LeftButton" and down == false then
-		vcbShowMenu()
-	end
-end)
 -- Move the Tabs --
 vcbOptions00:RegisterForDrag("LeftButton")
 vcbOptions00:SetScript("OnDragStart", vcbOptions00.StartMoving)
@@ -59,12 +39,3 @@ vcbOptions00:HookScript("OnHide", function(self)
 		if _G["vcbOptions"..i]:IsShown() then _G["vcbOptions"..i]:Hide() end
 	end
 end)
--- Events Time --
-local function EventsTime(self, event, arg1, arg2, arg3, arg4)
-	if event == "PLAYER_LOGIN" then
-		local category = Settings.RegisterCanvasLayoutCategory(self, "VCB")
-		category:SetName("|A:"..C_AddOns.GetAddOnMetadata("VCB", "IconAtlas")..":16:16|a "..C_AddOns.GetAddOnMetadata("VCB", "Title"))
-		Settings.RegisterAddOnCategory(category)
-	end
-end
-vcbOptions0:SetScript("OnEvent", EventsTime)
